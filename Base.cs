45e1defc8a3b0d857b9275b215a11dc266aca6c1
Wendell Mickael Cardoso <135742936+Oiquerida69@users.noreply.github.com>
2023-07-25 17:20:01 -0300

    Add files via upload

---
+++ Base.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtvProdutos
+{
+    internal class Base
+    {
+        public string nome;
+        public string descrição;
+        public int quantidade;
+        public double preço;
+        public bool status;
+
+        public void mostra()
+        {
+            Disponivel();
+            if (status == true)
+            {
+                Console.WriteLine($"[Produto] = {nome} [Quantidade] = {quantidade} [Descrição] = {descrição} [Valor] = {preço} [Status] = {status}");
+
+            }
+            else
+            {
+                Console.WriteLine($"[Produto] = {nome} [Quantidade] = {quantidade} [Descrição] = {descrição} [Valor] = {preço} [Status] = {status}");
+            }
+        }
+        public bool Disponivel()
+        {
+            if (quantidade <= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public void desativaProduto()
+        {
+            quantidade = 0;
+            status = false;
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using AtvProdutos;
+using System.Reflection.Metadata.Ecma335;
+
+Base Pipoca = new Base();
+Pipoca.nome = "Pipoca";
+Pipoca.quantidade = 10;
+Pipoca.descrição = "Milho para fazer pipoca";
+Pipoca.preço = 4.80;
+Pipoca.Disponivel();
+Pipoca.status = true;
+
+Pipoca.mostra();
+Console.ReadKey();
+
+Base Miojo = new Base();
+Miojo.nome = "Miojo";
+Miojo.quantidade = 20;
+Miojo.descrição = "Pacote de macarrão instantanêo";
+Miojo.preço = 2.50;
+Miojo.Disponivel();
+Miojo.status = true;
+
+Miojo.mostra();
+Console.ReadKey();
+
+Base Queijo = new Base();
+Queijo.nome = "Queijo";
+Queijo.quantidade = 12;
+Queijo.descrição = "Queijo mineiro de Otima qualidade";
+Queijo.preço = 7.50;
+Queijo.Disponivel();
+Queijo.status = true;
+
+Queijo.mostra();
+Console.ReadKey();
+
+Console.WriteLine("Digite o nome do produto que você deseja desativar");
+string resposta = Console.ReadLine();
+for (int contador = 0; contador < 2; contador++)
+{
+
+
+
+    switch (resposta)
+    {
+        case "Pipoca":
+            Pipoca.desativaProduto();
+            Pipoca.mostra();
+            break;
+
+        case "Miojo":
+            Miojo.desativaProduto();
+            Miojo.mostra();
+            break;
+
+        case "Queijo":
+            Queijo.desativaProduto();
+            Queijo.mostra();
+            break;
+        default:
+            return;
+    }
+  
+}
+Console.WriteLine("Digite o nome do produto que você deseja desativar");
+resposta = Console.ReadLine();
+for (int contador = 0; contador < 2; contador++)
+{
+
+
+
+    switch (resposta)
+    {
+        case "Pipoca":
+            Pipoca.desativaProduto();
+            Pipoca.mostra();
+            break;
+
+        case "Miojo":
+            Miojo.desativaProduto();
+            Miojo.mostra();
+            break;
+
+        case "Queijo":
+            Queijo.desativaProduto();
+            Queijo.mostra();
+            break;
+        default:
+            return;
+    }
+
+}
+
+
+
+
